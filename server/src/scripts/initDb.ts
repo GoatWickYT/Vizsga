2f3ebb15a098e9c040f130b2ca12e7171c206af4
Letargikus-oposszum <vidtomka@gmail.com>
2025-09-15 20:39:41 +0200

    ticket branch has been opened

--- server/src/scripts/initDb.ts
+++ server/src/scripts/initDb.ts
@@ -2,6 +2,7 @@ import db from '../config/db.js';
 
 async function initDb() {
     try {
+        //yes
         console.log('Database initialized successfully!');
     } catch (err) {
         console.error('Error initializing database:', err);
