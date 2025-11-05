eef2e13c5399fbfd9608d5f5be49294893b801ef
Wittner Attila <wiattila8@gmail.com>
2025-11-05 12:10:51 +0100

    base updated
    Added a navbar and a home page with basic styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import Home from './pages/Home';
+import './index.css';
+import Navbar from './pages/Navbar';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
+);
