4d93ab38ba3de81ee19a8b4db37a2ec57cca5410
Wittner Attila <wiattila8@gmail.com>
2025-11-05 14:07:01 +0100

    slight change

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import '../index.css';
 import './Home.css';
 
 const Home = () => {
--- apps/web/src/pages/Navbar.tsx
+++ apps/web/src/pages/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Navbar.css';
+import '../index.css';
 
 const Navbar = () => {
     const navigate = useNavigate();
