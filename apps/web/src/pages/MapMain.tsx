007eb007c568ae3d6d0045dd55bf85dbe6226559
Wittner Attila <wiattila8@gmail.com>
2025-11-05 16:46:32 +0100

    map started

---
+++ apps/web/src/pages/MapMain.tsx
@@ -0,0 +1,12 @@
+import './MapMain.css';
+import '../index.css';
+
+const MapMain = () => {
+    return (
+        <main className="MapMain">
+            <h1>Map</h1>
+        </main>
+    );
+};
+
+export default MapMain;
